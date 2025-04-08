05a777b570c2e59681987d7d76e7f3bb08f7d952
sivakuar <sivamadhan1901@gmail.com>
2025-04-08 17:32:05 +0530

    first commit

--- tests/iframe.spec.js
+++ tests/iframe.spec.js
@@ -1,10 +1,10 @@
-// import { test, expect } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-// test('I-frame', async ({ page }) => {
-//     await page.goto('https://rahulshettyacademy.com/AutomationPractice/#top');
+test.only('I-frame', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/#top');
     
-//     const frame = await page.frameLocator('iframe[name="iframe-name"]');
-//     await frame.locator('.navbar-collapse.collapse.clearfix ul li a[href="mentorship"]').first().click();
+    const frame = await page.frameLocator('iframe[name="iframe-name"]');
+    await frame.locator('.navbar-collapse.collapse.clearfix ul li a[href="mentorship"]').first().click();
 
-//     await page.pause();
-// });
+    await page.pause();
+});
